db90d6abcfb92b8247fabe0bf832ebcddf291f37
ken <82031953+ken-027@users.noreply.github.com>
2023-06-06 17:30:16 +0800

    RA-4 models and controllers

---
+++ app/Http/Controllers/InstructionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Instruction;
+use App\Http\Requests\StoreInstructionRequest;
+use App\Http\Requests\UpdateInstructionRequest;
+
+class InstructionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInstructionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Instruction $instruction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInstructionRequest $request, Instruction $instruction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Instruction $instruction)
+    {
+        //
+    }
+}
